e1b49e359be6ba69dfcf1abbba87e25b5accc69a
xenomorphdev <alamar1990@gmail.com>
2024-10-23 20:42:48 -0500

    Add template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@vesp/nuxt-fontawesome", "@nuxt/eslint"],
+  fontawesome: {
+    icons: {
+      solid: ["cog"],
+    },
+  },
+  eslint: {
+    // Aditional configs here
+  },
+});
